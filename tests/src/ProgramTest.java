24a1f769d9146bafec880afa51152bbd259adb17
DrNuenninger <tillmann@nuenninghoff.de>
2019-05-07 16:39:10 +0200

    Add test rental

---
+++ tests/src/ProgramTest.java
@@ -0,0 +1,24 @@
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class ProgramTest {
+
+    @Test
+    public void testRental() {
+        Movie m1 = new Movie("movie1", 1);
+        Movie m2 = new Movie("movie2", 2);
+        Rental r1 = new Rental(m1, 10);
+        Rental r2 = new Rental(m2, 5);
+        Customer c1 = new Customer("joe");
+        c1.addRental(r1);
+        c1.addRental(r2);
+        String result = c1.statement();
+        assertEquals("Rental Record for joe\n" +
+                "\tTitle\t\tDays\tAmount\n" +
+                "\tmovie1\t\t10\t30.0\n" +
+                "\tmovie2\t\t5\t4.5\n" +
+                "Amount owed is 34.5\n" +
+                "You earned 3 frequent renter points",result);
+    }
+}
